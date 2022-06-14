aa5f9fe128dd0fb0240f50a5e31c32c70b304d65
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-14 14:46:01 -0400

    included inquirer package in index.js; wrote questions array

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is your github username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your project description?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What is your project\'s usage?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'How can users install your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Who contributed to your project?',
+        name: 'contribution',
+    },
+    {
+        type: 'rawlist',
+        message: 'What license should be applied?',
+        choices: [
+            "MPL",
+            "GPL",
+            "Apache",
+            "MIT",
+            "CC",
+            "BSD",
+        ],
+        name: 'license',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
